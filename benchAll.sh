58110e3fc0f8c8ff2935db36958d1c59424d00a8
Björnborg Nguyen <bjornborg.nguyen@chalmers.se>
2018-11-10 23:37:34 +0100

    Bugfix

--- benchAll.sh
+++ benchAll.sh
@@ -8,10 +8,11 @@ projects=( deepflow dense-inverse-search dualtvl1 farneback lucas-kanade pcaflow
 for optflow in "${projects[@]}"
 do 
   echo -e "Running ${optflow}"
-  docker run -v ${1}}/data:/data bjornborg/${optflow}:latest ${2}
+  docker run -v ${1}:/data bjornborg/${optflow}:latest ${2}
 done
 
 if [[ $* == *-g* ]]; then
   echo "Using gpu on ${1}" 
+  echo -e "Running flownet2"
   docker run --runtime=nvidia ${1}:/data bjornborg/flownet2 -g 0 -n FlowNet2-KITTI ${2}
 fi
