38c7a7e5e80bd6ab108b3aed44ebc2b5dffb7811
Björnborg Nguyen <bjornborg.nguyen@chalmers.se>
2018-11-10 23:33:50 +0100

    Added gpu in the bench script

--- benchAll.sh
+++ benchAll.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+#$1 datapath
+#$2 num loops
 
-projects=( deepflow dense-inverse-search dualtvl1 farneback lucas-kanade pcaflow simpleflow flownet2 )
+projects=( deepflow dense-inverse-search dualtvl1 farneback lucas-kanade pcaflow simpleflow )
+
 
 for optflow in "${projects[@]}"
 do 
   echo -e "Running ${optflow}"
-  docker run -v ${PWD}/data:/data bjornborg/${optflow}:latest 3
+  docker run -v ${1}}/data:/data bjornborg/${optflow}:latest ${2}
 done
+
+if [[ $* == *-g* ]]; then
+  echo "Using gpu on ${1}" 
+  docker run --runtime=nvidia ${1}:/data bjornborg/flownet2 -g 0 -n FlowNet2-KITTI ${2}
+fi
