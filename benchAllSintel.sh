5a9dd81bcb7fb7f680b4de715bd6b4769b54ecb0
Björnborg Nguyen <bjornborg.nguyen@chalmers.se>
2019-02-26 19:47:46 +0100

    added beta tag

--- benchAllSintel.sh
+++ benchAllSintel.sh
@@ -7,10 +7,10 @@ projects=( deepflow dense-inverse-search farneback lucas-kanade pcaflow simplefl
 for optflow in "${projects[@]}"
 do 
   echo -e "Running ${optflow}"
-  docker run --runtime=nvidia --privileged -v /usr/bin/perf:/usr/bin/perf -v ${1}:/data bjornborg/${optflow}:latest ${2}
+  docker run --runtime=nvidia --privileged -v /usr/bin/perf:/usr/bin/perf -v ${1}:/data bjornborg/${optflow}:beta ${2}
 done
 if [[ $* == *-g* ]]; then
   # echo -e "Using gpu on ${1}" 
   echo -e "Running flownet2"
-  docker run --runtime=nvidia --privileged -v /usr/bin/perf:/usr/bin/perf -v ${1}:/data bjornborg/flownet2 -g 0 -n FlowNet2-CSS-Sintel ${2}
+  docker run --runtime=nvidia --privileged -v /usr/bin/perf:/usr/bin/perf -v ${1}:/data bjornborg/flownet2:beta -g 0 -n FlowNet2-CSS-Sintel ${2}
 fi
