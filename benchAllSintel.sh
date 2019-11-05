f389cc169c0b325437356b7bb92858886c6cb2ff
Björnborg Nguyen <bjornborg.nguyen@chalmers.se>
2019-11-05 23:34:40 +0800

    Update benchAllSintel.sh

--- benchAllSintel.sh
+++ benchAllSintel.sh
@@ -9,10 +9,10 @@ projects=( deepflow dense-inverse-search farneback lucas-kanade pcaflow simplefl
 for optflow in "${projects[@]}"
 do 
   echo -e "Running ${optflow}"
-  docker run --runtime=nvidia --privileged -v ${1}:/data bjornborg/${optflow}:${3} ${2}
+  docker run ---gpus all --privileged -v ${1}:/data bjornborg/${optflow}:${3} ${2}
 done
 
 if [[ $* == *-g* ]]; then
   echo -e "Running flownet2"
-  docker run --runtime=nvidia --cap-add SYS_ADMIN -v ${1}:/data bjornborg/flownet2:${3} -g 0 -n FlowNet2-CSS-Sintel ${2}
+  docker run --gpus all --cap-add SYS_ADMIN -v ${1}:/data bjornborg/flownet2:${3} -g 0 -n FlowNet2-CSS-Sintel ${2}
 fi
